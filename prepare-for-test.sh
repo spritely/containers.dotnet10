5297b571c49bad7c13d06645d0e55869f2d6ce04
Victor <2417328+vrobinson@users.noreply.github.com>
2025-12-10 15:24:27 -0500

    Initial project #minor

---
+++ prepare-for-test.sh
@@ -0,0 +1,12 @@
+container_name=$1
+copy_env_file=$2
+source_dir=./src
+destination_dir=test/$container_name/.devcontainer
+
+mkdir -p $destination_dir
+cp -r "$source_dir"/. "$destination_dir"
+mv "$destination_dir/container-compose.template" "$destination_dir/container-compose.yaml"
+if [ "$copy_env_file" = "true" ]; then
+  cp "test/$container_name/.devcontainer/.env.example" "test/$container_name/.devcontainer/.env"
+fi
+find "$destination_dir" -type f -exec sed -i "s/{repo-name}/test-${container_name}/g" {} +
